51f5f90b018d58fc2f28f521cee5b915966563d3
jonty <jontyfpine@gmail.com>
2025-03-11 14:55:58 +0000

    Implement lazy column for found locations

--- app/src/main/java/com/example/mapmissionary/HomePage.kt
+++ app/src/main/java/com/example/mapmissionary/HomePage.kt
@@ -1,10 +1,18 @@
 package com.example.mapmissionary
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -15,39 +23,51 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomePage(gridRefService: GridRefService) {
-    var userInput by remember { mutableStateOf("") }
+    var userInput by remember { mutableStateOf("Cardiff") }
     var listOfLocations by remember { mutableStateOf(listOf<Location>()) }
 
 
     Column(modifier = Modifier.fillMaxSize()) {
-        TextField(
-            value = userInput,
+        TextField(value = userInput,
             onValueChange = { userInput = it },
             label = { Text("Enter location name:") },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(top = 80.dp)
+                .padding(20.dp)
+//                .padding(top = 50.dp)
+                .fillMaxWidth()
                 .onFocusChanged { focusState ->
                     if (!focusState.isFocused) {
                         listOfLocations = gridRefService.getListOfLocations(userInput)
                     }
-                }
-        )
+                })
 
-        for (location in listOfLocations) {
-            Row(
-                modifier = Modifier
-                    .padding(10.dp)
-            ) {
-                Text(
-                    text = location.toString(),
-                )
+        LazyColumn(modifier = Modifier
+            .height(500.dp)
+        ) {
+            itemsIndexed(listOfLocations) { i, location ->
+                Row(
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    OutlinedCard(
+                        onClick = { Log.i("custom", "card clicked")}
+                    ) {
+                        Text(
+                            text = location.address.toString(), modifier = Modifier.padding(10.dp)
+                        )
+                    }
+
+                }
+
             }
         }
 
@@ -55,7 +75,7 @@ fun HomePage(gridRefService: GridRefService) {
             onClick = { listOfLocations = gridRefService.getListOfLocations(userInput) },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(100.dp),
+                .padding(vertical = 20.dp)
         ) {
             Text(
                 "Find",
