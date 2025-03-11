42b444a58dcf28e972ebf3c1a82dd47f6e09c052
jonty <jontyfpine@gmail.com>
2025-03-11 13:55:16 +0000

    Add on focus change logic for location input field

--- app/src/main/java/com/example/mapmissionary/HomePage.kt
+++ app/src/main/java/com/example/mapmissionary/HomePage.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,11 +33,18 @@ fun HomePage(gridRefService: GridRefService) {
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(top = 80.dp)
+                .onFocusChanged { focusState ->
+                    if (!focusState.isFocused) {
+                        listOfLocations = gridRefService.getListOfLocations(userInput)
+                    }
+                }
         )
 
         for (location in listOfLocations) {
-            Row(modifier = Modifier
-                .padding(10.dp)) {
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
                 Text(
                     text = location.toString(),
                 )
