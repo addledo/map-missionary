4cc1268af6accdb5e8d102018bc9aec8c9985d2c
jonty <jontyfpine@gmail.com>
2025-03-11 13:22:44 +0000

    Move postcode logic to separate class

--- app/src/main/java/com/example/mapmissionary/GridRefService.kt
+++ app/src/main/java/com/example/mapmissionary/GridRefService.kt
@@ -14,43 +14,11 @@ class GridRefService {
     }
 
     fun getListOfLocations(keyWords: String): List<Location> {
-        val validatedKeyWords = ensureValidPostcode(keyWords)
+        val validatedKeyWords = PostcodeValidator.validate(keyWords)
         val url = constructRequestUrl(validatedKeyWords)
         return fetchData(url)
     }
 
-    fun ensureValidPostcode(inputStr: String): String {
-        // The GeoDojo API only recognises postcodes with a space, this method checks for postcodes
-        // with missing spaces and adds one if necessary
-
-        //Regex for postcode found here
-        // https://stackoverflow.com/a/51885364
-        // Note original answer regex is "^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$"
-        // I have removed the " ?" from the middle in order to only detect postcodes without a space
-        // The outcode and incode are the first and second parts of the postcode, respectively
-        // I derived regexes for these from the above using lookahead and lookbehind
-        //TODO Write unit tests for this method
-
-        if (inputStr.length > 7) {
-            return inputStr
-        }
-
-        val spacelessPostcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?\\d[A-Z]{2}$")
-        val outcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?(?=\\d[A-Z]{2}$)")
-        val incodeRegex = Regex("(?<=^[A-Z]{1,2}\\d[A-Z\\d]?)\\d[A-Z]{2}$")
-
-        val inputIsInvalidPostcode = inputStr.uppercase().matches(spacelessPostcodeRegex)
-
-        if (inputIsInvalidPostcode) {
-            val invalidPostcode = inputStr.uppercase()
-            val incode = incodeRegex.find(invalidPostcode)?.value
-            val outcode = outcodeRegex.find(invalidPostcode)?.value
-            val validPostcode = "$outcode $incode"
-            Log.i("custom", "Invalid postcode found: $inputStr, replaced with $validPostcode")
-            return validPostcode
-        }
-        return inputStr
-    }
 
     private fun fetchData(urlString: String): List<Location> {
         val url = URL(urlString)
---
+++ app/src/main/java/com/example/mapmissionary/PostcodeValidator.kt
@@ -0,0 +1,38 @@
+package com.example.mapmissionary
+
+import android.util.Log
+
+// The GeoDojo API only recognises postcodes with a space, this method checks for postcodes
+// with missing spaces and adds one if necessary
+
+//Regex for postcode found here
+// https://stackoverflow.com/a/51885364
+// Note original answer regex is "^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$"
+// I have removed the " ?" from the middle in order to only detect postcodes without a space
+// The outcode and incode are the first and second parts of the postcode, respectively
+// I derived regexes for these from the above using lookahead and lookbehind
+//TODO Write unit tests
+
+object PostcodeValidator {
+    private val spacelessPostcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?\\d[A-Z]{2}$")
+    private val outcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?(?=\\d[A-Z]{2}$)")
+    private val incodeRegex = Regex("(?<=^[A-Z]{1,2}\\d[A-Z\\d]?)\\d[A-Z]{2}$")
+
+    fun validate(inputStr: String): String {
+        if (inputStr.length > 7) {
+            return inputStr
+        }
+
+        val inputIsInvalidPostcode = inputStr.uppercase().matches(spacelessPostcodeRegex)
+
+        if (inputIsInvalidPostcode) {
+            val invalidPostcode = inputStr.uppercase()
+            val incode = incodeRegex.find(invalidPostcode)?.value
+            val outcode = outcodeRegex.find(invalidPostcode)?.value
+            val validPostcode = "$outcode $incode"
+            Log.i("custom", "Invalid postcode found: $inputStr, replaced with $validPostcode")
+            return validPostcode
+        }
+        return inputStr
+    }
+}
