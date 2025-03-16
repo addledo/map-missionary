6d2cbb868e5b93b41ff0eaec3596a63ac7723efb
addledo <jontyfpine@gmail.com>
2025-03-16 20:44:34 +0000

    Fix formatting bug.
    Fix bug where trailing 0s and '.'s are removed too eagerly making values like 10.0 become 1

--- app/src/main/java/com/example/mapmissionary/data/LatLong.kt
+++ app/src/main/java/com/example/mapmissionary/data/LatLong.kt
@@ -2,15 +2,25 @@ package com.example.mapmissionary.data
 
 data class LatLong(val lat: Double, val long: Double) {
 
-
     override fun toString(): String {
         val decimalPlacesForOneMeterAccuracy = 5
-        val format = "%.${decimalPlacesForOneMeterAccuracy}f"
+        val dpFormat = "%.${decimalPlacesForOneMeterAccuracy}f"
 
-        // Rounds to specified DP and then removes trailing 0s
-        // Also removes the decimal point if there are no numbers after it
-        val latStr = format.format(lat).trimEnd { it == '0' || it == '.' }
-        val longStr = format.format(long).trimEnd { it == '0' || it == '.' }
+        // Rounds to n dp
+        var latStr = dpFormat.format(lat)
+        var longStr = dpFormat.format(long)
+
+        // Removes trailing 0s to remove redundant information without reducing accuracy
+        // This could potentially imply imprecision but I have decided to format this
+        // way in order to make the values easier to read and write
+        latStr = latStr.trimEnd { it == '0' }
+        longStr = longStr.trimEnd { it == '0' }
+
+        // Removes trailing decimal point if there are no digits following
+        // e.g. "10." becomes "10"
+        // Must be done separately to the previous operation or 10.0 becomes 1
+        latStr = latStr.trimEnd { it == '.' }
+        longStr = longStr.trimEnd { it == '.' }
 
         return "$latStr, $longStr"
     }
