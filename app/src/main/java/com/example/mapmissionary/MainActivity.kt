5cb7b14d9731dc7d08e33d35dd0e8ad7b1d1178f
jonty <jontyfpine@gmail.com>
2025-03-18 15:32:35 +0000

    Extract navigation logic

--- app/src/main/java/com/example/mapmissionary/MainActivity.kt
+++ app/src/main/java/com/example/mapmissionary/MainActivity.kt
@@ -3,14 +3,8 @@ package com.example.mapmissionary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.example.mapmissionary.navigation.AppNavigation
 import com.example.mapmissionary.ui.theme.MapMissionaryTheme
-import com.example.mapmissionary.views.CurrentLocationScreen
-import com.example.mapmissionary.views.LocationDetailsScreen
-import com.example.mapmissionary.views.LocationSearchScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,17 +18,5 @@ class MainActivity : ComponentActivity() {
                 AppNavigation()
             }
         }
-    }
-}
-
-
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "search") {
-        composable("search") { LocationSearchScreen(navController) }
-        composable("location_details") { LocationDetailsScreen(navController) }
-        composable("current_location") { CurrentLocationScreen(navController) }
     }
 }
---
+++ app/src/main/java/com/example/mapmissionary/navigation/Navigation.kt
@@ -0,0 +1,21 @@
+package com.example.mapmissionary.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mapmissionary.views.CurrentLocationScreen
+import com.example.mapmissionary.views.LocationDetailsScreen
+import com.example.mapmissionary.views.LocationSearchScreen
+
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "search") {
+        composable(route = Screen.Search.route) { LocationSearchScreen(navController) }
+        composable(route = Screen.LocationDetails.route) { LocationDetailsScreen(navController) }
+        composable(route = Screen.CurrentLocation.route) { CurrentLocationScreen(navController) }
+    }
+}
