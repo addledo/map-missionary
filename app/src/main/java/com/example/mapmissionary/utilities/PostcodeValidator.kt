3a42e369cdd4d1d8081a70f80ebf1ea23b0495ca
jonty <jontyfpine@gmail.com>
2025-03-18 17:51:25 +0000

    Tidy PostcodeValidator

--- app/src/main/java/com/example/mapmissionary/utilities/PostcodeValidator.kt
+++ app/src/main/java/com/example/mapmissionary/utilities/PostcodeValidator.kt
@@ -11,28 +11,34 @@ import android.util.Log
 // I have removed the " ?" from the middle in order to only detect postcodes without a space
 // The outcode and incode are the first and second parts of the postcode, respectively
 // I derived regexes for these from the above using lookahead and lookbehind
-//TODO Write unit tests
 
 object PostcodeValidator {
     private val spacelessPostcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?\\d[A-Z]{2}$")
     private val outcodeRegex = Regex("^[A-Z]{1,2}\\d[A-Z\\d]?(?=\\d[A-Z]{2}$)")
     private val incodeRegex = Regex("(?<=^[A-Z]{1,2}\\d[A-Z\\d]?)\\d[A-Z]{2}$")
+    private const val MAXIMUM_POSTCODE_LENGTH = 7
 
-    fun validate(inputStr: String): String {
-        if (inputStr.length > 7) {
-            return inputStr
+    fun validate(string: String): String {
+        if (string.length > MAXIMUM_POSTCODE_LENGTH) {
+            return string
         }
 
-        val inputIsInvalidPostcode = inputStr.uppercase().matches(spacelessPostcodeRegex)
-
-        if (inputIsInvalidPostcode) {
-            val invalidPostcode = inputStr.uppercase()
-            val incode = incodeRegex.find(invalidPostcode)?.value
-            val outcode = outcodeRegex.find(invalidPostcode)?.value
-            val validPostcode = "$outcode $incode"
-            Log.i("custom", "Invalid postcode found: $inputStr, replaced with $validPostcode")
-            return validPostcode
+        if (string.isBadPostcode()) {
+            return reformatPostcode(string)
         }
-        return inputStr
+        return string
+    }
+
+    private fun reformatPostcode(postcode: String): String {
+        val invalidPostcode = postcode.uppercase()
+        val incode = incodeRegex.find(invalidPostcode)?.value
+        val outcode = outcodeRegex.find(invalidPostcode)?.value
+        val validPostcode = "$outcode $incode"
+        Log.d("postcode", "Invalid postcode found: $postcode, replaced with $validPostcode")
+        return validPostcode
+    }
+
+    private fun String.isBadPostcode(): Boolean {
+        return this.uppercase().matches(spacelessPostcodeRegex)
     }
 }
