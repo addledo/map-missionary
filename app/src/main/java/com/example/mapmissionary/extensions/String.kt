4a2ec255a353a47da395b762ec99f2b9fa81b1fb
jonty <jontyfpine@gmail.com>
2025-04-01 17:41:01 +0100

    Fix uneven grid reference length bug

--- app/src/main/java/com/example/mapmissionary/extensions/String.kt
+++ app/src/main/java/com/example/mapmissionary/extensions/String.kt
@@ -4,7 +4,7 @@ package com.example.mapmissionary.extensions
 fun String.isGridRef(): Boolean {
     val gridRefRegex = Regex("[a-zA-Z]{2}\\d{1,5}\\s?\\d{1,5}")
 
-    if (this.length > 13) {
+    if (this.length > 13 || this.length % 2 != 0) {
         return false
     }
 
